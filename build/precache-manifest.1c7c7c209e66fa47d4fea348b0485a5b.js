d8a96451a2e5dd2dddbac7604dce454d99c03b74
daritimm <dariustimmer@gmail.com>
2020-09-21 01:56:29 +0200

    added build

---
+++ build/precache-manifest.1c7c7c209e66fa47d4fea348b0485a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191727e41557750058f8081b3e8644ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eba874e47f177a3f76f1",
+    "url": "/static/js/2.20b8b417.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.20b8b417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29cc064f41021f56df7c",
+    "url": "/static/js/main.63a0a597.chunk.js"
+  },
+  {
+    "revision": "b447d8cf4fe0deecba8f",
+    "url": "/static/js/runtime-main.1d7e3ca8.js"
+  },
+  {
+    "revision": "550c8d982765df8b38d1752ad32eb3ce",
+    "url": "/static/media/bilbo.550c8d98.jpg"
+  }
+]);
